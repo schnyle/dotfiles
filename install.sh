eaa6fbabee46ba8f2a185d4e315f9f437888572e
Kyle <kylesch115@gmail.com>
2025-03-07 09:09:56 -0700

    add manual install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+ln -sf ~/.dotfiles/i3/.config/i3/config ~/.config/i3/config
+
+ln -sf ~/.dotfiles/picom/.config/picom.conf ~/.config/picom.conf
+
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
+
+ln -sf ~/.dotfiles/vscode/.config/Code/User/settings.json ~/.config/Code/User/settings.json
