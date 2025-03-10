548ba09052738327a1c756db76e39a65bc8b0b4e
Kyle <kylesch115@gmail.com>
2025-03-10 13:23:37 -0600

    add bashrc

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+ln -sf ~/.dotfiles/alacritty/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+ln -sf ~/.dotfiles/bash/.bashrc ~/.bashrc
+
 ln -sf ~/.dotfiles/i3/.config/i3/config ~/.config/i3/config
 
 ln -sf ~/.dotfiles/picom/.config/picom.conf ~/.config/picom.conf
@@ -6,5 +10,3 @@ ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
 ln -sf ~/.dotfiles/vscode/.config/Code/User/settings.json ~/.config/Code/User/settings.json
-
-ln -sf ~/.dotfiles/alacritty/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
