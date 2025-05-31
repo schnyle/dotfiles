d2c3a3095fff72c8f5dd2f58a2b5e013ee3949bc
kyle <kylesch115@gmail.com>
2025-05-31 17:45:56 -0600

    update to use gnu stow

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotfiles
+
+The dotfiles for my system
+
+## Requirements
+
+- stow: `pacman -S stow`
+
+## Installation
+
+Execute `install.sh`
--- install.sh
+++ install.sh
@@ -1,21 +1,11 @@
-ln -sf ~/.dotfiles/alacritty/.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+#!/bin/bash
 
-ln -sf ~/.dotfiles/bash/.bashrc ~/.bashrc
+cd "$(dirname $0)"
 
-ln -sf ~/.dotfiles/i3/.config/i3/config ~/.config/i3/config
-ln -sf ~/.dotfiles/i3/.config/i3status/config ~/.config/i3status/config
+for dir in */; do
+  if [[ "$dir" == "ignore/" ]]; then
+    continue
+  fi
 
-ln -sf ~/.dotfiles/nvim/.config/nvim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/.dotfiles/nvim/.config/nvim/lua/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua
-ln -sf ~/.dotfiles/nvim/.config/nvim/lua/config/plugins/mini.lua ~/.config/nvim/lua/config/plugins/mini.lua
-
-ln -sf ~/.dotfiles/picom/.config/picom.conf ~/.config/picom.conf
-
-ln -sf ~/.dotfiles/qutebrowser/.config/qutebrowser/config.py ~/.config/qutebrowser/config.py
-
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
-
-ln -sf ~/.dotfiles/vscode/.config/Code/User/settings.json ~/.config/Code/User/settings.json
-
-ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+  stow $dir -t ~/test-stow
+done
