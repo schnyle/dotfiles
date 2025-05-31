de232087722a947d28a6d48ddad0c24750243c72
kyle <kylesch115@gmail.com>
2025-05-31 16:45:51 -0600

    add nvim

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ ln -sf ~/.dotfiles/bash/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/i3/.config/i3/config ~/.config/i3/config
 ln -sf ~/.dotfiles/i3/.config/i3status/config ~/.config/i3status/config
 
+ln -sf ~/.dotfiles/nvim/.config/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/.dotfiles/nvim/.config/nvim/lua/config/lazy.lua ~/.config/nvim/lua/config/lazy.lua
+ln -sf ~/.dotfiles/nvim/.config/nvim/lua/config/plugins/mini.lua ~/.config/nvim/lua/config/plugins/mini.lua
+
 ln -sf ~/.dotfiles/picom/.config/picom.conf ~/.config/picom.conf
 
 ln -sf ~/.dotfiles/qutebrowser/.config/qutebrowser/config.py ~/.config/qutebrowser/config.py
