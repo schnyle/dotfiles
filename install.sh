a44ac0bffdba54affd3510e96e3f9402c008cf23
kyle <kylesch115@gmail.com>
2025-10-17 21:10:51 -0600

    on install: remove config file if already exists

--- install.sh
+++ install.sh
@@ -3,9 +3,16 @@
 cd "$(dirname $0)"
 
 for dir in */; do
+  # skip files in ignore/ (mac config files)
   if [[ "$dir" == "ignore/" ]]; then
     continue
   fi
+
+  # remove config file if already exists
+  find "$dir" -type f | while read file; do
+    target="$HOME/${file#*/}"
+    [[ -f "$target" ]] && rm "$target"
+  done
 
   stow $dir -R
 done
